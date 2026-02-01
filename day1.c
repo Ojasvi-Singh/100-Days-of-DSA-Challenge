246b920d117a17baf77efb5d1d303fac9a6f751a
Ojasvi-Singh <84854325+Ojasvi-Singh@users.noreply.github.com>
2026-02-01 17:14:56 +0530

    Add C program to insert element in array

---
+++ day1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int arr[100];
+    int n, pos, x, i;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &pos);
+    scanf("%d", &x);
+    for (i = n; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+    arr[pos - 1] = x;
+    n++;
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
