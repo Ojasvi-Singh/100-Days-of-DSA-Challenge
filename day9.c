5e4e9d68888112dc4d28dee1242cdec027867852
Ojasvi-Singh <84854325+Ojasvi-Singh@users.noreply.github.com>
2026-02-09 10:18:35 +0530

    Create day9.c
    Removed comments to clean up the code.

---
+++ day9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[1001]; 
+    scanf("%s", str);
+    int len = strlen(str);
+    int start = 0;
+    int end = len - 1;
+    char temp;
+
+    while (start < end) {
+        temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+    printf("%s\n", str);
+
+    return 0;
+}
